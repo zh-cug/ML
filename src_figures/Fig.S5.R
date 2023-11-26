70344999a49843d575d1418e6bdb10a90de5df25
zh-cug <53112203+zh-cug@users.noreply.github.com>
2023-11-26 20:03:06 +0000

    Update Fig.S5.R

--- src_figures/Fig.S5.R
+++ src_figures/Fig.S5.R
@@ -5,10 +5,9 @@ setwd("/Users/zhenghuang/Desktop/Processing/ML-CMAQ/")
 load("./Figdata/td.Rdata")
 load("./Figdata/ts.Rdata")
 
-
 models<-c("CMAQ", "GC", "KZ", "MLR", "RF", "XGB")
 vars<-c("LT", "EMI", "MET")
-pd<-list()
+figdata<-list()
 for (i in 1:6){
   for (j in 1:3){
     data<-subset(ts, model==models[i])[, c("date", "city",  vars[j])]
@@ -20,18 +19,15 @@ for (i in 1:6){
     data<-data[, c("date", "mean", "low", "up")]
     data$model<-models[i]
     data$var<-vars[j]
-    pd[[(i-1)*6+j]]<-data
+    figdata[[(i-1)*6+j]]<-data
     print(j)
   }
   print(models[i])
 }
-pd<-do.call(rbind, pd)  
+figdata<-do.call(rbind, figdata)  
 
-
-a<-ggplot(subset(pd, var=="LT"), aes(date, mean, colour=model))+geom_line()+
+a<-ggplot(subset(figdata, var=="LT"), aes(date, mean, colour=model))+geom_line()+
   geom_ribbon(aes(ymax=up, ymin=low, fill=model, colour=model), alpha=0.4, colour=NA)+
- # scale_fill_manual(values = c("#f03b20", "#d95f0e", "#2c7fb8", "#31a354", "#c51b8a", "#dd1c77"))+
-  #scale_colour_manual(values = c("#f03b20", "#d95f0e", "#2c7fb8", "#31a354", "#c51b8a", "#dd1c77"))+
   theme_bw()+
   theme(legend.position = c(0.01, 0.99),
         legend.justification = c(0, 1),
@@ -44,10 +40,8 @@ a<-ggplot(subset(pd, var=="LT"), aes(date, mean, colour=model))+geom_line()+
   ylab(expression("Scaled PM" [2.5]^OBS))
 
 
-b<-ggplot(subset(pd, var=="EMI"), aes(date, mean, colour=model))+geom_line()+
+b<-ggplot(subset(figdata, var=="EMI"), aes(date, mean, colour=model))+geom_line()+
   geom_ribbon(aes(ymax=up, ymin=low, fill=model, colour=model), alpha=0.4, colour=NA)+
-  #scale_fill_manual(values = c("#f03b20", "#d95f0e", "#2c7fb8", "#31a354", "#c51b8a", "#dd1c77"))+
-  #scale_colour_manual(values = c("#f03b20", "#d95f0e", "#2c7fb8", "#31a354", "#c51b8a", "#dd1c77"))+
   theme_bw()+
   theme(legend.position = "none",
         axis.title = element_text(size=9),
@@ -67,4 +61,4 @@ c<-ggplot(subset(pd, var=="MET"), aes(date, mean, colour=model))+geom_line()+
   ylab(expression("Scaled PM"[2.5]^MET))
 cowplot::plot_grid(a, b, c, ncol=1)
 
-export::graph2jpg(file="./Figs/Fig.S5_.jpg", width=9/2.54, height=18/2.54)
+export::graph2jpg(file="Fig.S5.jpg", width=9/2.54, height=18/2.54)
