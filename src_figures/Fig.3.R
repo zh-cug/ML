b572427cc98e41f7b718a63bd3b3f2d80cd9b1d9
zh-cug <53112203+zh-cug@users.noreply.github.com>
2023-11-26 19:52:19 +0000

    Update Fig.3.R

--- src_figures/Fig.3.R
+++ src_figures/Fig.3.R
@@ -4,38 +4,12 @@ library(ggsignif)
 library(ggpmisc)
 library(reshape2)
 
-setwd("/Users/zhenghuang/Desktop/Processing/ML-CMAQ/")
-load("./Figdata/mp.Rdata")
-load("./Figdata/td.Rdata")
-
-
-
+setwd("./Figdata/")
+load("mp.Rdata")
+load("td.Rdata")
 
 fig.3a<-subset(mp, model%in%c("RF", "rf_nt") & res=="h")
-#fig.4a$NMB<-fig.4a$NMB*100
 fig.3a<-melt(fig.3a, measure.vars = c("r", "NMB", "IOA"), variable.name = "var", value.name = "value")
-
-nmb<-ggplot(subset(mp, model%in%c("RF", "rf_nt") & res=="h"), aes(model, NMB, fill=model))+
-  stat_boxplot(geom = "errorbar",width=0.5, position=position_dodge(0.8))+
-  geom_boxplot(width=0.5, position=position_dodge(0.8))+
-  stat_summary(mapping = aes(group=model), position=position_dodge(0.8), fun.y="mean",
-               geom="point", shape=23, size=2, fill="white")+
-  scale_fill_manual(values = c("#619CFF", "gray"), 
-                    labels=c("tu", "ut"),
-                    breaks = c("RF", "rf_nt"))+
-  stat_signif(comparisons = list(c("RF", "rf_nt")),
-              map_signif_level = T,
-              textsize = 2)+
-  theme_bw()+
-  theme(legend.position = "none",
-        axis.title = element_blank(),
-        axis.text = element_text(size=8),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        panel.grid = element_blank())+
-  ylim(-0.006, 0.019)
-
-
 
 a<-ggplot(fig.3a, aes(var, value, fill=model))+
   stat_boxplot(geom = "errorbar",width=0.5, position=position_dodge(0.8))+
@@ -62,9 +36,6 @@ a<-ggplot(fig.3a, aes(var, value, fill=model))+
   ylab("Value")+
   xlab("Statistics")+
   ylim(c(0.6, 1.03))
-
-
-
 
 fig.3b<-subset(td, model%in%c("RF", "rf_nt"))
 fig.3b$var<-factor(fig.3b$var, levels = c("LT", "EMI", "MET"))
@@ -101,7 +72,6 @@ b<-ggplot(fig.3b, aes(var, slope, fill=model))+
 fig.3c<-dcast(subset(td, model%in%c("CMAQ", "RF", "rf_nt")), city+var~model, value.var = "slope")
 fig.3c<-melt(fig.3c, measure.vars = 4:5, variable.name = "model", value.name = "trend")
 
-
 c<-ggplot(subset(fig.3c, var=="EMI"), aes(CMAQ, trend, colour=model, fill=model))+
   geom_point( shape=1)+
   stat_smooth(method = "lm",show.legend = F)+
@@ -128,7 +98,4 @@ c<-ggplot(subset(fig.3c, var=="EMI"), aes(CMAQ, trend, colour=model, fill=model)
 
 cowplot::plot_grid(a, b, c, ncol=3, align = "vh", labels = letters[1:3], label_size = 9)
 
-export::graph2pdf(file="./Manuscript/revision_2nd/Figures/Fig.3.pdf", width=19/2.54, height=6.5/2.54)
-
-
-
+export::graph2pdf(file="Fig.3.pdf", width=19/2.54, height=6.5/2.54)
